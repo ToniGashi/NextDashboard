5238002d071b70ba8205d621e708c6d6ab3f9922
ToniGashi <tonigashi999@gmail.com>
2023-11-11 14:02:35 +0100

    removing cookie on sign out

--- app/ui/dashboard/topnav.tsx
+++ app/ui/dashboard/topnav.tsx
@@ -4,6 +4,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 import Search from '../search';
+import { cookies } from 'next/headers';
 
 export default function TopNav() {
   return ( 
@@ -18,6 +19,7 @@ export default function TopNav() {
       <div className="flex flex-row justify-between space-x-2 rounded-3xl items-center gap-2 p-1 bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:h-[48px] md:w-fit md:p-3 md:px-3 md:flex-col md:space-x-0 md:mx-10 md:space-y-2">
         <form action={async () => {
           'use server';
+          cookies().delete('userId');
           await signOut();
         }}>
           <button className='flex w-full items-center justify-center'>
